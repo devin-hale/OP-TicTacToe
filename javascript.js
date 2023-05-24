d8355a8dc9f03d6f6d2cedce640941aabb1ecf6d
dwhale2 <devinwaynehale@gmail.com>
2023-05-24 14:16:40 -0500

    Added reset function, tied to reset button.

--- javascript.js
+++ javascript.js
@@ -23,18 +23,29 @@ const gameBoard = (() => {
             gbDiv.appendChild(gameSquare);
             gameSquare.addEventListener('click', a => {
                 if (item.written == true) {
-
                 } else {
-                a.target.innerHTML = 'X'
-                a.target.dataset.value = 'X'
-                a.target.dataset.written = true
-                a.target.classList.remove('unwritten');
+                    a.target.innerHTML = 'X'
+                    a.target.dataset.value = 'X'
+                    a.target.dataset.written = true
+                    a.target.classList.remove('unwritten');
                 }
             }) 
         })
     }
 
-    return {getArray, writeArray, generate};
+    const erase = () => {
+        Array.from(document.getElementById('gameBoard').children).forEach( (item, index) => {
+            item.dataset.value = '';
+            gameArray[index].value = '';
+            item.dataset.written = false;
+            gameArray[index].written = false;
+            item.classList.add('unwritten');
+            item.innerHTML = '';
+        })
+    }
+
+    return {getArray, writeArray, generate, erase};
 })();
 
 gameBoard.generate();
+document.getElementById('resetButton').addEventListener('click', () => gameBoard.erase());
